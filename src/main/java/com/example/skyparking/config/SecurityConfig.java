92334b5d8756964b83fe476f8f7372df4d3c9c48
AManych <anderss1918>
2022-01-13 16:49:52 +0200

    work authorization with login

--- src/main/java/com/example/skyparking/config/SecurityConfig.java
+++ src/main/java/com/example/skyparking/config/SecurityConfig.java
@@ -1,22 +1,43 @@
 package com.example.skyparking.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                    .csrf()
-                    .disable()
-                    .authorizeRequests()
-                    .antMatchers("/createTalon")
-                    .authenticated()
+        http.httpBasic().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/home")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
                 .and()
-                    .oauth2Login();
+                .formLogin()
+                .and()
+                .logout().logoutSuccessUrl("/");
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
